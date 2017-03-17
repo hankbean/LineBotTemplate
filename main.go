528758496e5077260777a0304f697205d2fd2850
hankbean <hank200702@gmail.com>
2017-03-18 03:16:08 +0800

    1

--- main.go
+++ main.go
@@ -17,7 +17,8 @@ import (
 	"log"
 	"net/http"
 	"os"
-
+	"math/rand"
+	
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
@@ -51,7 +52,30 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 				case *linebot.TextMessage:
 					//if _, err = 
 					if message.Text=="早餐"{
-						bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("去吃屎吧")).Do()
+						rand.Seed(42) // Try changing this number!
+						answers := []string{
+							"It is certain",
+							"It is decidedly so",
+							"Without a doubt",
+							"Yes definitely",
+							"You may rely on it",
+							"As I see it yes",
+							"Most likely",
+							"Outlook good",
+							"Yes",
+							"Signs point to yes",
+							"Reply hazy try again",
+							"Ask again later",
+							"Better not tell you now",
+							"Cannot predict now",
+							"Concentrate and ask again",
+							"Don't count on it",
+							"My reply is no",
+							"My sources say no",
+							"Outlook not so good",
+							"Very doubtful",
+	}
+						bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(answers[rand.Intn(len(answers)))).Do()
 					} else if message.Text=="午餐"{
 						bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("不要吃啦")).Do()
 					} else {

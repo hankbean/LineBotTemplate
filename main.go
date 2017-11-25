360db2982c99d7ba1435ceda06b63c4465fa6447
hankbean <hank200702@gmail.com>
2017-11-25 13:59:18 +0800

    名單更新

--- main.go
+++ main.go
@@ -232,11 +232,17 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 					} else if message.Text=="祥瑀" || message.Text=="黃祥瑀"{
 						bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("約炮小王子")).Do()
 
-					} else if message.Text=="于姿婷"{
+					} else if message.Text=="于姿婷" || message.Text=="黃玥萱"{
 						bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("烤土司小公主")).Do()
 
 					} else if message.Text=="博榮" || message.Text=="榮榮" || message.Text=="王博榮"{
 						bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("封杯小王子")).Do()
+
+					} else if message.Text=="躍萱" || message.Text=="黃躍萱"{
+						bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("封杯小王子")).Do()
+
+					} else if message.Text=="萱萱"{
+						bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("你找躍萱還是玥萱")).Do()
 
 					} else {
 						//bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("指令清單\r\n"+"早餐/午餐/晚餐\r\n今日運勢")).Do()
